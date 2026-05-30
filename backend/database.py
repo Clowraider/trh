27db9a5ac70e70e99457478b7f49e3f5134d5bed
Clowraider <trainerhdm@gmail.com>
2026-05-30 09:15:28 -0300

    chore(cleanup): remove legacy backend/frontend tree and old docs

--- backend/database.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
-from config import DB_CONFIG
-
-
-def get_connection():
-    # RealDictCursor hace que los resultados vengan como diccionarios
-    # en lugar de tuplas, mas facil de convertir a JSON
-    return psycopg2.connect(**DB_CONFIG, cursor_factory=RealDictCursor)
